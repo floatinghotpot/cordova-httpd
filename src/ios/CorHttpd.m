6576436ab0bfc6de57ebae0c948c55db5e30adb7
Liming Xie <liming.xie@gmail.com>
2014-07-16 22:47:10 +0800

    code for ios works now for relative path, not working for / yet

--- src/ios/CorHttpd.m
+++ src/ios/CorHttpd.m
@@ -2,101 +2,105 @@
 
 #import <Cordova/CDV.h>
 
+#import "DDLog.h"
+#import "DDTTYLogger.h"
+#import "HTTPServer.h"
+
 @interface CorHttpd : CDVPlugin {
     // Member variables go here.
 
 }
 
-//@property(nonatomic, retain) WebServer *httpd;
-@property(nonatomic, retain) NSString *wwwRoot;
+@property(nonatomic, retain) HTTPServer *httpServer;
+@property(nonatomic, retain) NSString *localPath;
+@property(nonatomic, retain) NSString *url;
 @property (assign) int port;
 
 - (void)startServer:(CDVInvokedUrlCommand*)command;
 - (void)stopServer:(CDVInvokedUrlCommand*)command;
+- (void)getURL:(CDVInvokedUrlCommand*)command;
+- (void)getLocalPath:(CDVInvokedUrlCommand*)command;
+
+- (NSDictionary *)getIPAddresses;
+- (NSString *)getIPAddress:(BOOL)preferIPv4;
+
 @end
 
-#if 0 /*
-
-#import "HTTPServer.h"
-#import "DDLog.h"
-#import "DDTTYLogger.h"
-
-// Log levels: off, error, warn, info, verbose
-static const int ddLogLevel = LOG_LEVEL_VERBOSE;
-
-
-@implementation iPhoneHTTPServerAppDelegate
-
-@synthesize window;
-@synthesize viewController;
-
-- (void)startServer
-{
-    // Start the server (and check for problems)
-	
-	NSError *error;
-	if([httpServer start:&error])
-	{
-		DDLogInfo(@"Started HTTP Server on port %hu", [httpServer listeningPort]);
-	}
-	else
-	{
-		DDLogError(@"Error starting HTTP Server: %@", error);
-	}
-}
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-	// Configure our logging framework.
-	// To keep things simple and fast, we're just going to log to the Xcode console.
-	[DDLog addLogger:[DDTTYLogger sharedInstance]];
-	
-	// Create server using our custom MyHTTPServer class
-	httpServer = [[HTTPServer alloc] init];
-	
-	// Tell the server to broadcast its presence via Bonjour.
-	// This allows browsers such as Safari to automatically discover our service.
-	[httpServer setType:@"_http._tcp."];
-	
-	// Normally there's no need to run our server on any specific port.
-	// Technologies like Bonjour allow clients to dynamically discover the server's port at runtime.
-	// However, for easy testing you may want force a certain port so you can just hit the refresh button.
-	// [httpServer setPort:12345];
-	
-	// Serve files from our embedded Web folder
-	NSString *webPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Web"];
-	DDLogInfo(@"Setting document root: %@", webPath);
-	
-	[httpServer setDocumentRoot:webPath];
-    
-    [self startServer];
-    
-    // Add the view controller's view to the window and display.
-    [window addSubview:viewController.view];
-    [window makeKeyAndVisible];
-    
-    return YES;
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    [self startServer];
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // There is no public(allowed in AppStore) method for iOS to run continiously in the background for our purposes (serving HTTP).
-    // So, we stop the server when the app is paused (if a users exits from the app or locks a device) and
-    // restart the server when the app is resumed (based on this document: http://developer.apple.com/library/ios/#technotes/tn2277/_index.html )
-    
-    [httpServer stop];
-}
-
-*/
-
-#endif
-
 @implementation CorHttpd
+
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+#define IOS_CELLULAR    @"pdp_ip0"
+#define IOS_WIFI        @"en0"
+#define IP_ADDR_IPv4    @"ipv4"
+#define IP_ADDR_IPv6    @"ipv6"
+
+- (NSString *)getIPAddress:(BOOL)preferIPv4
+{
+    NSArray *searchArray = preferIPv4 ?
+    @[ IOS_WIFI @"/" IP_ADDR_IPv4, IOS_WIFI @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6 ] :
+    @[ IOS_WIFI @"/" IP_ADDR_IPv6, IOS_WIFI @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4 ] ;
+    
+    NSDictionary *addresses = [self getIPAddresses];
+    NSLog(@"addresses: %@", addresses);
+    
+    __block NSString *address;
+    [searchArray enumerateObjectsUsingBlock:^(NSString *key, NSUInteger idx, BOOL *stop)
+     {
+         address = addresses[key];
+         if(address) *stop = YES;
+     } ];
+    return address ? address : @"0.0.0.0";
+}
+
+- (NSDictionary *)getIPAddresses
+{
+    NSMutableDictionary *addresses = [NSMutableDictionary dictionaryWithCapacity:8];
+    
+    // retrieve the current interfaces - returns 0 on success
+    struct ifaddrs *interfaces;
+    if(!getifaddrs(&interfaces)) {
+        // Loop through linked list of interfaces
+        struct ifaddrs *interface;
+        for(interface=interfaces; interface; interface=interface->ifa_next) {
+            if(!(interface->ifa_flags & IFF_UP) /* || (interface->ifa_flags & IFF_LOOPBACK) */ ) {
+                continue; // deeply nested code harder to read
+            }
+            const struct sockaddr_in *addr = (const struct sockaddr_in*)interface->ifa_addr;
+            char addrBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
+            if(addr && (addr->sin_family==AF_INET || addr->sin_family==AF_INET6)) {
+                NSString *name = [NSString stringWithUTF8String:interface->ifa_name];
+                NSString *type;
+                if(addr->sin_family == AF_INET) {
+                    if(inet_ntop(AF_INET, &addr->sin_addr, addrBuf, INET_ADDRSTRLEN)) {
+                        type = IP_ADDR_IPv4;
+                    }
+                } else {
+                    const struct sockaddr_in6 *addr6 = (const struct sockaddr_in6*)interface->ifa_addr;
+                    if(inet_ntop(AF_INET6, &addr6->sin6_addr, addrBuf, INET6_ADDRSTRLEN)) {
+                        type = IP_ADDR_IPv6;
+                    }
+                }
+                if(type) {
+                    NSString *key = [NSString stringWithFormat:@"%@/%@", name, type];
+                    addresses[key] = [NSString stringWithUTF8String:addrBuf];
+                }
+            }
+        }
+        // Free memory
+        freeifaddrs(interfaces);
+    }
+    return [addresses count] ? addresses : nil;
+}
+
+- (void)pluginInitialize
+{
+    self.httpServer = nil;
+    self.localPath = @"";
+    self.url = @"";
+}
 
 - (void)startServer:(CDVInvokedUrlCommand*)command
 {
@@ -104,32 +108,92 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
     NSString* wwwRoot = [command.arguments objectAtIndex:0];
     int port = [[command.arguments objectAtIndex:1] intValue];
 
-    if (wwwRoot != nil) {
-        
-        /* start web server */
-        
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    if(self.httpServer != nil) {
+        if([self.httpServer isRunning]) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"server is already up"];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            return;
+        }
     }
-
+    
+    if (wwwRoot == nil) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"wwwRoot is missing"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    self.httpServer = [[HTTPServer alloc] init];
+    
+    // Tell the server to broadcast its presence via Bonjour.
+    // This allows browsers such as Safari to automatically discover our service.
+    //[self.httpServer setType:@"_http._tcp."];
+    
+    // Normally there's no need to run our server on any specific port.
+    // Technologies like Bonjour allow clients to dynamically discover the server's port at runtime.
+    // However, for easy testing you may want force a certain port so you can just hit the refresh button.
+    // [httpServer setPort:12345];
+    
+    [self.httpServer setPort:port];
+    
+    // Serve files from our embedded Web folder
+    const char * str = [wwwRoot UTF8String];
+    if(*str == '/') {
+        self.localPath = wwwRoot;
+    } else {
+        NSString* basePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"www"];
+        self.localPath = [NSString stringWithFormat:@"%@/%@", basePath, wwwRoot];
+    }
+    NSLog(@"Setting document root: %@", self.localPath);
+    [self.httpServer setDocumentRoot:self.localPath];
+    
+	NSError *error;
+	if([self.httpServer start:&error]) {
+        int listenPort = [self.httpServer listeningPort];
+		NSLog(@"Started httpd on port %d", listenPort);
+        
+        NSString* ip = [self getIPAddress:YES];
+        self.url = [NSString stringWithFormat:@"http://%@:%d/", ip, listenPort];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:self.url];
+        
+	} else {
+		NSLog(@"Error starting httpd: %@", error);
+        
+        NSString* errmsg = [error description];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:errmsg];
+	}
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)stopServer:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    NSString* reservedArg = [command.arguments objectAtIndex:0];
-    
-    if (reservedArg != nil) {
+    if(self.httpServer != nil) {
         
-        /* stop web server */
+        [self.httpServer stop];
+        self.httpServer = nil;
         
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        self.localPath = @"";
+        self.url = @"";
+        
+        NSLog(@"httpd stopped");
     }
     
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getURL:(CDVInvokedUrlCommand *)command
+{
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                      messageAsString:(self.url ? self.url : @"")];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getLocalPath:(CDVInvokedUrlCommand *)command
+{
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                      messageAsString:(self.localPath ? self.localPath : @"")];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
